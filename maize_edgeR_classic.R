acb6dfe3533c985dcfb46c2fa47f234999f6327e
Surya Saha <ss2489@cornell.edu>
2014-02-07 17:40:52 -0500

    Fixed typos and added viz steps

--- maize_edgeR_classic.R
+++ maize_edgeR_classic.R
@@ -1,14 +1,17 @@
 mzCounts <- read.delim("maize_counts_noaxenic_noheader.txt",header=FALSE,row.names=1)
-colSums(mzCounts) /1e06 #mill reads per lib
+coltf = floor(colSums(mzCounts) /1e06) #mill reads per lib
 mzCountsCln = subset(mzCounts,V2>coltf[1] & V3>coltf[2] & V4>coltf[3] & V5>coltf[4] & V6>coltf[5] & V7>coltf[6] & V8>coltf[7] & V9>coltf[8]& V10>coltf[9]& V11>coltf[10]& V12>coltf[11]& V13>coltf[12]& V14>coltf[13]& V15>coltf[14]& V16>coltf[15]& V17>coltf[16]& V18>coltf[17]& V19>coltf[18]& V20>coltf[19]& V21>coltf[20]& V22>coltf[21]& V23>coltf[22]& V24>coltf[23]& V25>coltf[24])
+groups<- c("3d", "3d", "7d", "7d", "3dctrl", "10dctrl", "3dctrl", "10dctrl", "3d", "10d", "5dctrl", "3dctrl", "10dctrl", "10d", "10d", "7d", "5d", "5d", "5dctrl", "5dctrl", "7dctr", "7dctrl", "7dctrl", "5d")
 
-group<- c("3d", "3d", "7d", "7d", "3dctrl", "10dctrl", "3dctrl", "10dctrl", "3d", "10d", "5dctrl", "3dctrl", "10dctrl", "10d", "10d", "7d", "5d", "5d", "5dctrl", "5dctrl", "7dctr", "7dctrl", "7dctrl", "5d")
 hist(coltcln, labels=TRUE,xlab="Million reads in sample",main="Histogram of reads mapped per sample")
 barplot(coltcln,names.arg=sort(group), xlab="Library name", ylab="Read count",las=2,col="yellow",main="Number of million reads mapped per sample")
 
-dge <- DGEList(count=mzCountsCln,group=condition)
+dge <- DGEList(count=mzCountsCln,group=groups)
 dge <- calcNormFactors(dge) # normalize libs to prevent over expressed genes from blanking out rest
-plotMDS(dge,labels=group,col=c("darkblue","lightblue","darkgreen","lightgreen","darkred","red","black","gray")[factor(group)]) #diff colors for each group
+
+barplot(dge$counts,names.arg=sort(groups),las=2,main="Normalized maize counts per library")
+plotMDS(dge,labels=groups,col=c("darkblue","lightblue","darkgreen","lightgreen","darkred","red","black","gray")[factor(groups)]) #diff colors for each group
+
 dge <- estimateCommonDisp(dge) # Maximizes the negative binomial conditional common likelihood to give the estimate of the common dispersion across all tags
 dge <- estimateTagwiseDisp(dge) # Estimates tagwise dispersion values by an empirical Bayes method based on weighted conditional maximum likelihood.
 plotMeanVar(dge,show.tagwise.vars=TRUE,NBline=TRUE) #each dot represents the estimated mean and variance for each gene, with binned variances as well as the trended common dispersion overlaid. Explore the mean-variance relationship for DGE data
@@ -19,8 +22,20 @@ dge_5 = exactTest(dge,pair=c('5d','5dctrl'))
 dge_7 = exactTest(dge,pair=c('7d','7dctrl'))
 dge_10 = exactTest(dge,pair=c('10d','10dctrl'))
 
-dge_3 = exactTest(dge,pair=c('3d','3dctrl')) #Extracts the top DE tags in a data frame for a given pair of groups, ranked by p-value or absolute log-fold change
-dge_5 = exactTest(dge,pair=c('5d','5dctrl'))
+dge_3_tt = topTags(dge_3,n=nrow(dge_3)) #Extracts the top DE tags in a data frame for a given pair of groups, ranked by p-value or absolute log-fold change
+dge_3_tt_matrix = as.matrix(dge_3_tt$table)
+rc <- rainbow(nrow(dge_3_tt_matrix), start = 0, end = .3)
+cc <- rainbow(ncol(dge_3_tt_matrix), start = 0, end = .3)
+#poor viz, first try
+cc <- rainbow(ncol(dge_3_tt_matrix), start = 0, end = .3)
+rc <- rainbow(nrow(dge_3_tt_matrix), start = 0, end = .3)
+hv <- heatmap(dge_3_tt_matrix, col = cm.colors(256), scale = "column",
+              RowSideColors = rc, ColSideColors = cc, margins = c(5,10),
+              xlab = " variables", ylab =  "Genes",
+              main = "heatmap ")
+heatmap.2(dge_3_tt_matrix) #better, should try for pval < .05 only
+
+dge_5_tt = topTags(dge_5,n=nrow(dge_5))
 dge_7_tt = topTags(dge_7,n=nrow(dge_7))
 dge_10_tt = topTags(dge_10,n=nrow(dge_10))
 
