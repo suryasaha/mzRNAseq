bb60c391d5680eedd7bff88d17d0d3d1749ef9c4
Surya Saha <ss2489@cornell.edu>
2014-02-26 10:29:09 -0500

    Added creds

--- sanity_check.R
+++ sanity_check.R
@@ -1,5 +1,15 @@
+# PPath@Cornell
+# Surya Saha ss2489@cornell.edu
+# Feb 17, 2014
+
+
 mzErrCounts <- read.delim("maize_errors.txt",header=FALSE,row.names=1)
 fgErrCounts <- read.delim("fungi_errors.txt",header=FALSE,row.names=1)
 
 barplot(colSums(fnErrCounts), xlab="Library name", ylab="Read count",col="yellow",main="Number of maize reads mapped to St28A transcripts per sample")
 barplot(colSums(fgErrCounts), names.arg=sort(fgErrCountGroups),xlab="Library name", ylab="Read count",col="yellow",main="Number of maize reads mapped to St28A transcripts per sample",las=2)
+
+rawCounts = read.columns(file="raw_counts.txt",sep="\t")
+
+
+#TODO fungi/maize counts per library or condition
