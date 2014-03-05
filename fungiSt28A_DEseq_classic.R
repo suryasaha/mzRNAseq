4a6e0df7199624d8ee0787d6d9cf579e5e1efc89
Surya Saha <ss2489@cornell.edu>
2014-03-05 10:14:17 -0500

    Ran DESeq workflow

--- fungiSt28A_DEseq_classic.R
+++ fungiSt28A_DEseq_classic.R
@@ -11,7 +11,9 @@
 
 #data
 fnCounts <- read.delim("fungi_counts_header_nocontrol.txt",header=TRUE,row.names=1)
-design = data.frame(row.names = colnames (fnCounts), condition = c("3d", "3d", "7d", "7d", "3d", "10d", "10d", "10d", "axenic", "axenic","7d", "axenic", "5d", "5d", "5d"))
+design = data.frame(row.names = colnames (fnCounts), 
+                    condition = c("3d", "3d", "7d", "7d", "3d", "10d", "10d", "10d", "axenic", "axenic","7d", "axenic", "5d", "5d", "5d"),
+                    libType = c("paired","paired","paired","paired","paired","paired","paired","paired","paired","paired","paired","paired","paired","paired","paired"))
 
 #filtering: DESeq suggests using a quantile method. Instead I'll use the method used with edgeR for comparison purposes.
 #removing genes with < 1 read/million reads in every library, 15 libs
@@ -28,3 +30,68 @@ table(use)
 #use
 #FALSE  TRUE 
 #5852  5850  
+
+#DEseq
+library("DESeq")
+cds = newCountDataSet(fnCountsFl,design)
+cds = estimateSizeFactors (cds) #est normalization factors
+sizeFactors(cds) #print norm factor for each lib, large ones are axenic
+#OYNG        OYNH        OYNN        OYNO        OYNW        OYNX        OYOB 
+#0.15877283  0.04864184  0.25830195  0.38721655  0.08752413  3.44974220  1.76835773 
+#OYOC        OYOG        OYOH        OYON        OYOO        OYOP        OYOS 
+#1.57892713 38.23951728 68.24167231  1.90249824 47.37053757  0.18604037  0.46115122 
+#OYOZ 
+#0.11882388
+
+#For PCA plot
+cdsFullBlind = estimateDispersions(cds, method = "blind")
+#Error in parametricDispersionFit(means, disps) : 
+#  Parametric dispersion fit failed. Try a local fit and/or a pooled estimation. (See '?estimateDispersions')
+cdsFullBlindLocal = estimateDispersions(cds, method = "blind",fitType="local") #Used the locfit package to fit a dispersion-mean relation due to error
+vsdFullLocal = varianceStabilizingTransformation (cdsFullBlindLocal)
+plotPCA(vsdFullLocal,intgroup=c("condition","libType"))
+
+#For Est Disps
+cds = estimateDispersions(cds)
+#OLD ERROR due to missing meta-data in design
+#Error in if (nr < 2) stop("nrow(modelMatrix) must be >=2.") : 
+#  argument is of length zero
+plotDispEsts(cds)
+
+#heatmap of indiv genes
+library("RColorBrewer")
+library ("gplots")
+select = order(rowMeans(counts(cds)), decreasing = TRUE)[1:3000]  
+hmcol = colorRampPalette(brewer.pal(8, "GnBu"))(100)
+heatmap.2(exprs(vsdFullLocal)[select,], col = hmcol, trace="none", margin=c(10,6),labRow=NA)
+heatmap.2(exprs(vsdFullLocal)[select,], col = redblue(75), trace="none", margin=c(10,6),labRow=NA,labCol=design$condition)
+
+#heatmap of dataset to dataset
+dists = dist( t( exprs(vsdFullLocal) ) )
+mat = as.matrix( dists )
+rownames(mat) = colnames(mat) = with(pData(cdsFullBlindLocal), paste(rownames(design), design$condition, sep=" : "))
+heatmap.2(mat, trace="none", col = rev(redblue(75)), margin=c(13, 13))
+
+#generate DE tables (comparison is expt to axenic)
+res10toax = nbinomTest (cds, "10d", "axenic")
+plotMA(res10toax)
+#Error in MA[, array] - x : non-numeric argument to binary operator
+#In addition: There were 50 or more warnings (use warnings() to see the first 50)
+#> warnings()
+#Warning messages:
+#  1: In mean.default(sort(x, partial = half + 0L:1L)[half +  ... :
+#         argument is not numeric or logical: returning NA
+#  2: In mean.default(sort(x, partial = half + 0L:1L)[half +  ... :
+#         argument is not numeric or logical: returning NA
+res7toax = nbinomTest (cds, "7d", "axenic")
+res5toax = nbinomTest (cds, "5d", "axenic")
+res3toax = nbinomTest (cds, "3d", "axenic")
+write.csv(res10toax,"fungi_10d_alltags_deseq.csv")
+write.csv(res7toax,"fungi_7d_alltags_deseq.csv")
+write.csv(res5toax,"fungi_5d_alltags_deseq.csv")
+write.csv(res3toax,"fungi_3d_alltags_deseq.csv")
+
+#shell
+#awk -F"," '{if($4 < .05) print $1}' fungi_5d_alltags_edgeR.csv | sed 's,\",,g'>edgeR
+#awk -F"," '{if($9 < .05) print $2}' fungi_5d_alltags_deseq.csv| sed 's,\",,g' > adj
+#awk -F"," '{if($8 < .05) print $2}' fungi_5d_alltags_deseq.csv | sed 's,\",,g' > raw
