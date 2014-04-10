2cc97526b2d80f01efbd215e332b83499dff9cd4
Surya Saha <ss2489@cornell.edu>
2014-04-09 23:21:46 -0400

    Repeated with correct filtering

--- fungiSt28A_edgeR_classic.R
+++ fungiSt28A_edgeR_classic.R
@@ -8,20 +8,39 @@
 #cut --complement -f 2,3,8-11,14-16,25-29 fungi_counts.txt > fungi_counts_nocontrol_noheader.txt 
 
 fnCounts <- read.delim("fungi_counts_nocontrol_noheader.txt",header=FALSE,row.names=1)
-hist(floor(colSums(fnCounts)), labels=TRUE,xlab="Reads in sample",main="Histogram of reads mapped per sample")
+hist(floor(colSums(fnCounts)), labels=TRUE,xlab="Reads in sample",
+     main="Histogram of reads mapped per sample")
 coltf = floor(colSums(fnCounts) /1e06) #mill reads per lib
-hist(coltf, labels=TRUE,xlab="Million reads in sample",main="Histogram of reads mapped per sample")
+hist(coltf, labels=TRUE,xlab="Million reads in sample",
+     main="Histogram of reads mapped per sample")
+collabels = c("3d1", "3d2", "7d1", "7d2", "3d3", "10d1", "10d2", "10d3", "axenic1",
+              "axenic2","7d3", "axenic3", "5d1", "5d2", "5d3")
+colnames(fnCounts) = collabels
+#boxplot of all counts, las just print X axis labels vertically
+boxplot(fnCounts,main="Spread of all counts per library - St28A", las=2)
+
 
 #filter out low counts
-#removing reads with < 1 read/million reads in library
-fnCountsCln = subset(fnCounts,V2>coltf[1] & V3>coltf[2] & V4>coltf[3] & V5>coltf[4] & V6>coltf[5] & V7>coltf[6] & V8>coltf[7] & V9>coltf[8]& V10>coltf[9]& V11>coltf[10]& V12>coltf[11]& V13>coltf[12]& V14>coltf[13]& V15>coltf[14]& V16>coltf[15])
-coltfcln = floor(colSums(fnCountsCln) /1e06) #mill reads per lib
-hist(coltfcln, labels=TRUE,xlab="Million reads in sample",main="Histogram of reads mapped per sample after removing low counts")
-sort(floor(colSums(fnCountsCln)))
+#Initially removed genes with < 1 read/million in ANY library
+# > dim(fnCounts[rowSums(cpm(fnCounts) >1) ==15,])
+# [1] 3597   15
+# fnCountsCln = subset(fnCounts,V2>coltf[1] & V3>coltf[2] & V4>coltf[3] & V5>coltf[4] & V6>coltf[5] & V7>coltf[6] & V8>coltf[7] & V9>coltf[8]& V10>coltf[9]& V11>coltf[10]& V12>coltf[11]& V13>coltf[12]& V14>coltf[13]& V15>coltf[14]& V16>coltf[15])
+# coltfcln = floor(colSums(fnCountsCln) /1e06) #mill reads per lib
+# hist(coltfcln, labels=TRUE,xlab="Million reads in sample",main="Histogram of reads mapped per sample after removing low counts")
+# sort(floor(colSums(fnCountsCln)))
+
+#tutorial method removes only genes with < 1 read/million in 3 or less libs. 
+#3 being the (min) number of replicates
+# > dim(fnCounts[rowSums(cpm(fnCounts) >1) >=3,])
+# [1] 9856   15
+fnCountsCln = fnCounts[rowSums(cpm(fnCounts) >1) >=3,] #get subset
+boxplot(fnCountsCln,main="Spread of filtered counts per library - St28a", las=2)
+
 
 #labels
 groups<- c("3d", "3d", "7d", "7d", "3d", "10d", "10d", "10d", "axenic", "axenic","7d", "axenic", "5d", "5d", "5d")
-barplot(coltfcln,names.arg=groups, xlab="Library name", ylab="Read count",las=2,col="yellow",main="Number of million reads mapped per sample")
+barplot(colSums(fnCountsCln),names.arg=groups, xlab="Library name", ylab="Read count",las=2,col="yellow",
+        main="Nof reads mapped per sample filtered- St28A")
 
 #edgeR
 library("edgeR")
@@ -30,20 +49,26 @@ names(dge)
 #[1] "counts"             "samples"            "common.dispersion"  "pseudo.counts"      "AveLogCPM"         
 #[6] "pseudo.lib.size"    "prior.n"            "tagwise.dispersion"
 dge <- calcNormFactors(dge) # normalize libs to prevent over expressed genes from blanking out rest
-barplot(dge$counts,names.arg=groups,las=2,main="Normalized fungal counts per library")
+barplot(dge$counts,names.arg=groups,las=2,main="Normalized fungal counts/library St28A")
+#VIZ
+#http://cgrlucb.wikispaces.com/edgeR+spring2013
+library(RColorBrewer)
+colors <- brewer.pal(9, "Set1")
+#If we want the normalized pseudo-counts, useful for instance for cluster analysis,
+scale = dge$samples$lib.size * dge$samples$norm.factors
+normCounts = round(t(t(dge$counts)/scale)*mean(scale))
+boxplot(log2(normCounts+1), las=2, col=colors[dge$samples$group],
+        main="Normalized counts - St28A")
 plotMDS(dge,labels=groups,col=c("darkblue","darkgreen","darkred","black","orange")[factor(groups)]) #diff colors for each group
 
 dge <- estimateCommonDisp(dge) # Maximizes the negative binomial conditional common likelihood to give the estimate of the common dispersion across all tags
 dge <- estimateTagwiseDisp(dge) # Estimates tagwise dispersion values by an empirical Bayes method based on weighted conditional maximum likelihood.
-plotMeanVar(dge,show.tagwise.vars=TRUE,NBline=TRUE) #each dot represents the estimated mean and variance for each gene, with binned variances as well as the trended common dispersion overlaid. Explore the mean-variance relationship for DGE data
-plotBCV(dge) #Biological Coefficient of Variation
+plotMeanVar(dge,show.tagwise.vars=TRUE,NBline=TRUE, main="edgeR St28A") #each dot represents the estimated mean and variance for each gene, with binned variances as well as the trended common dispersion overlaid. Explore the mean-variance relationship for DGE data
+plotBCV(dge, main="edgeR St28A") #Biological Coefficient of Variation
 
 # Compute genewise exact tests for differences in the means between 
 # two groups of negative-binomially distributed counts
 dge_3 = exactTest(dge,pair=c('3d','axenic'))
-#Warning message:
-#In mglmOneGroup(y, dispersion = dispersion, offset = offset) :
-#  max iteractions exceeded for 1 tags
 dge_5 = exactTest(dge,pair=c('5d','axenic'))
 dge_7 = exactTest(dge,pair=c('7d','axenic'))
 dge_10 = exactTest(dge,pair=c('10d','axenic'))
@@ -57,22 +82,24 @@ dge_nc <- cpm(dge,normalized.lib.sizes=TRUE) #Computes counts per million (CPM)
 head(dge_nc[rownames(dge_3_tt$table),order(groups)],5) # depth-adjusted reads per million for some of the top 5 differentially expressed genes on day 3
 head(dge_nc[rownames(dge_5_tt$table),order(groups)],5) # depth-adjusted reads per million for some of the top 5 differentially expressed genes on day 5
 
-plotSmear(dge,de.tags=rownames(dge_3_tt$table$FDR < .05),main="Fungi 3days, log-Fold Change versus log-Concentration for FDR < .05")
-plotSmear(dge,de.tags=rownames(dge_5_tt$table$FDR < .05),main="Fungi 5days, log-Fold Change versus log-Concentration for FDR < .05")
-plotSmear(dge,de.tags=rownames(dge_7_tt$table$FDR < .05),main="Fungi 7days, log-Fold Change versus log-Concentration for FDR < .05")
-plotSmear(dge,de.tags=rownames(dge_10_tt$table$FDR < .05),main="Fungi 10days, log-Fold Change versus log-Concentration for FDR < .05")
+plotSmear(dge,de.tags=rownames(dge_3_tt$table$FDR < .05),main="Fungi 3d, log-Fold Change vs log-Conc FDR < .05")
+plotSmear(dge,de.tags=rownames(dge_5_tt$table$FDR < .05),main="Fungi 5d, log-Fold Change vs log-Conc FDR < .05")
+plotSmear(dge,de.tags=rownames(dge_7_tt$table$FDR < .05),main="Fungi 7d, log-Fold Change vs log-Conc FDR < .05")
+plotSmear(dge,de.tags=rownames(dge_10_tt$table$FDR < .05),main="Fungi 10d, log-Fold Change vs log-Conc FDR < .05")
 
-#VIZ
+write.csv(dge_3_tt$table, file="edgeR_fungi_3d_alltags.csv")
+write.csv(dge_5_tt$table, file="edgeR_fungi_5d_alltags.csv")
+write.csv(dge_7_tt$table, file="edgeR_fungi_7d_alltags.csv")
+write.csv(dge_10_tt$table, file="edgeR_fungi_10d_alltags.csv")
 
-#http://cgrlucb.wikispaces.com/edgeR+spring2013
-library(RColorBrewer)
-colors <- brewer.pal(9, "Set1")
+#shell
+# awk -F"," '{if($4 < .05) print }' fungi_3d_alltags_edgeR.csv | wc -l
+# awk -F"," '{if($4 < .05) print }' fungi_5d_alltags_edgeR.csv | wc -l
+# awk -F"," '{if($4 < .05) print }' fungi_7d_alltags_edgeR.csv | wc -l
+# awk -F"," '{if($4 < .05) print }' fungi_10d_alltags_edgeR.csv | wc -l
 
-#If we want the normalized pseudo-counts, useful for instance for cluster analysis,
-scale = dge$samples$lib.size*dge$samples$norm.factors
-normCounts = round(t(t(dge$counts)/scale)*mean(scale))
-boxplot(log2(normCounts+1), las=2, col=colors[dge$samples$group],names=groups,main="Normalized counts")
 
+#VIZ TODO
 # day3
 top <- topTags(dge_3, n=nrow(cds$counts))$table
 de <- rownames(top[top$FDR<0.05,])
@@ -86,14 +113,3 @@ heatmap.2(log(normCounts[de[1:400],]+1), ColSideColor=colors[groups],
           labCol=groups, main="Top 400 St28A genes DE \non day 3 FDR < 0.05", labRow=NA)
 heatmap.2(log(normCounts), ColSideColor=colors[groups],
           labCol=groups, main="All St28A  genes", labRow=NA)
-
-write.csv(dge_3_tt$table, file="fungi_3d_alltags_edgeR.csv")
-write.csv(dge_5_tt$table, file="fungi_5d_alltags_edgeR.csv")
-write.csv(dge_7_tt$table, file="fungi_7d_alltags_edgeR.csv")
-write.csv(dge_10_tt$table, file="fungi_10d_alltags_edgeR.csv")
-
-#shell
-# awk -F"," '{if($4 < .05) print }' fungi_3d_alltags_edgeR.csv | wc -l
-# awk -F"," '{if($4 < .05) print }' fungi_5d_alltags_edgeR.csv | wc -l
-# awk -F"," '{if($4 < .05) print }' fungi_7d_alltags_edgeR.csv | wc -l
-# awk -F"," '{if($4 < .05) print }' fungi_10d_alltags_edgeR.csv | wc -l
