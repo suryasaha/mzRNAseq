e4a607a661fa19e65d8cb3c8fbd1069ab7315058
Surya Saha <ss2489@cornell.edu>
2014-08-21 15:27:11 -0400

    Init

---
+++ maize_DEseq2_classic.R
@@ -0,0 +1,45 @@
+# PPath@Cornell
+# Surya Saha ss2489 at cornell dot edu
+# Aug 19, 2014
+
+#63540 maize transcripts in 24 diff conditions ( 4 time points x 3 reps x ( 1 expt + 1 control )).
+
+#shell
+#grep -v '^jgi' counts.txt > maize_counts.txt
+#cut --complement -f 2,3,19,20,22  maize_counts.txt > maize_counts_noaxenic.txt
+
+#################################################################
+#data
+#################################################################
+mzCounts <- read.delim("maize_counts_noaxenic.txt",header=TRUE,row.names=1)
+# this design trips up nbiomTest, using simple condition vector
+# design = data.frame(row.names = colnames (mzCounts), 
+#                     condition = c("3d", "3d", "7d", "7d", "3dctrl", "10dctrl", "3dctrl", "10dctrl", "3d", "10d", "5dctrl", "3dctrl", "10dctrl", "10d", "10d", "7d", "5d", "5d", "5dctrl", "5dctrl", "7dctrl", "7dctrl", "7dctrl", "5d" ),
+#                     libType = c("paired","paired","paired","paired","paired","paired","paired","paired","paired","paired","paired","paired","paired","paired","paired","paired","paired","paired","paired","paired","paired","paired","paired","paired"))
+condition = c("3d", "3d", "7d", "7d", "3dctrl", "10dctrl", "3dctrl", "10dctrl", "3d", "10d", "5dctrl", "3dctrl", "10dctrl", "10d", "10d", "7d", "5d", "5d", "5dctrl", "5dctrl", "7dctrl", "7dctrl", "7dctrl", "5d" )
+
+
+########### filtering ##############
+#removing genes with < 1 read/million reads in every library, 24 libs
+library("edgeR")
+keepcpm = rowSums (cpm(mzCounts)>1) >= 24
+dim(mzCounts[keepcpm, ]) 
+#[1] 17357    24
+#filter out low counts
+mzCountsFlCPM = mzCounts[keepcpm, ]
+
+#quantile counts 
+rs <- rowSums(mzCounts)
+#remove the genes in the lowest 50% quantile (as indicated by the parameter theta)
+theta <- 0.5
+keeptheta <- rs >quantile(rs, probs = theta)
+table(keeptheta)
+#use
+#FALSE  TRUE 
+#31778 31762 
+mzCountsFlTheta = mzCounts[keeptheta,]
+
+#################################################################
+#DESeq2 
+#################################################################
+library("DESeq2") #version 1.4.5
