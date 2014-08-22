3a5b3ededf1f393863d4e46bfe675b3368489dd3
Surya Saha <ss2489@cornell.edu>
2014-08-21 22:57:28 -0400

    Added DESeq2 CPM>1 and Theta 0.5 code

--- maize_DEseq2_classic.R
+++ maize_DEseq2_classic.R
@@ -45,7 +45,82 @@ table(keeptheta)
 #31778 31762
 mzCountsFlTheta = mzCounts[keeptheta,]
 
+#################################################################
+#DESeq2
+#################################################################
+library("DESeq2") #version 1.4.5
 
+ddsmzCountFlCPM = DESeqDataSetFromMatrix(
+  countData = mzCountsFlCPM,
+  colData = mzMeta,
+  design = ~ condition
+)
+
+ddsmzCountFlCPM = DESeq(ddsmzCountFlCPM)
+# estimating size factors
+# estimating dispersions
+# gene-wise dispersion estimates
+# mean-dispersion relationship
+# final dispersion estimates
+# fitting model and testing
+# Warning message:
+#   In estimateDispersionsFit(object, fitType = fitType, quiet = quiet) :
+#   the parametric fit of dispersion estimates over the mean of counts
+# failed, which occurs when the trend is not well captured by the
+# function y = a/x + b. A local regression fit is automatically performed,
+# and the analysis can continue. You can specify fitType='local' or 'mean'
+# to avoid this message if re-running the same data.
+# When using local regression fit, the user should examine plotDispEsts(dds)
+# to make sure the fitted line is not sharply curving up or down based on
+# the position of individual points.
+png("/home/surya/work/RJN_GT_Setur_Maize/R/maize_seseq2_plotDispEsts_ddsmzCountFlCPM.png")
+plotDispEsts(ddsmzCountFlCPM, main = "plotDispEsts_ddsmzCountFlCPM_localfit")
+dev.off()
+resmzCountFlCPM10 = results(ddsmzCountFlCPM, contrast = c("condition", "10day", "10daycontrol"))
+nrow(resmzCountFlCPM[which(resmzCountFlCPM$padj <= 0.05),])
+# [1] 9744
+resmzCountFlCPM7 = results(ddsmzCountFlCPM, contrast = c("condition", "7day", "7daycontrol"))
+resmzCountFlCPM5 = results(ddsmzCountFlCPM, contrast = c("condition", "5day", "5daycontrol"))
+resmzCountFlCPM3 = results(ddsmzCountFlCPM, contrast = c("condition", "3day", "3daycontrol"))
+nrow(resmzCountFlCPM5[which(resmzCountFlCPM5$padj <= 0.05),])
+# [1] 5397
+# day 5 is again low, like in deseq
+nrow(resmzCountFlCPM3[which(resmzCountFlCPM3$padj <= 0.05),])
+# [1] 10086
+nrow(resmzCountFlCPM7[which(resmzCountFlCPM7$padj <= 0.05),])
+# [1] 9483
+
+
+
+
+ddsmzCountFlTheta = DESeqDataSetFromMatrix(
+  countData = mzCountsFlTheta,
+  colData = mzMeta,
+  design = ~ condition
+)
+ddsmzCountFlTheta = DESeq(ddsmzCountFlTheta)
+# estimating size factors
+# estimating dispersions
+# gene-wise dispersion estimates
+# mean-dispersion relationship
+# final dispersion estimates
+# fitting model and testing
+png("/home/surya/work/RJN_GT_Setur_Maize/R/maize_seseq2_plotDispEsts_ddsmzCountFlTheta.png")
+plotDispEsts(ddsmzCountFlCPM, main = "plotDispEsts_ddsmzCountFlTheta_Parafit")
+dev.off()
+resmzCountFlTheta10 = results(ddsmzCountFlTheta, contrast = c("condition", "10day", "10daycontrol"))
+nrow(resmzCountFlTheta10[which(resmzCountFlTheta10$padj <= 0.05),])
+# [1] 14582
+nrow(resmzCountFlTheta7[which(resmzCountFlTheta7$padj <= 0.05),])
+resmzCountFlTheta7 = results(ddsmzCountFlTheta, contrast = c("condition", "7day", "7daycontrol"))
+nrow(resmzCountFlTheta7[which(resmzCountFlTheta7$padj <= 0.05),])
+# [1] 14258
+resmzCountFlTheta5 = results(ddsmzCountFlTheta, contrast = c("condition", "5day", "5daycontrol"))
+nrow(resmzCountFlTheta5[which(resmzCountFlTheta5$padj <= 0.05),])
+# [1] 7097
+resmzCountFlTheta3 = results(ddsmzCountFlTheta, contrast = c("condition", "3day", "3daycontrol"))
+nrow(resmzCountFlTheta3[which(resmzCountFlTheta3$padj <= 0.05),])
+# [1] 14678
 
 
 
